6bd237ba5ce7443283ece10c97eb0f34e65fed9c
Guillem <guillem@disedit.com>
2025-04-18 15:11:38 +0200

    add custom route

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -6,4 +6,14 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::page.page');
+module.exports = createCoreController('api::page.page' , ({ strapi }) => ({
+  async findOne({ slug }) {
+    const entity = await strapi.db.query('api::page.page').findOne({
+      where: { slug },
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
